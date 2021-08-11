2ed7b2ce13b2c8bac709f251cbcd5a006a24b262
Lewis Maina <muriukimainalewis@gmail.com>
2021-08-11 09:25:52 +0300

    Setup PostgreSQL as DB engine

--- bloggr/wsgi.py
+++ bloggr/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bloggr project.
+WSGI config for bloggrgr project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloggr.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloggrgr.settings")
 
 application = get_wsgi_application()
